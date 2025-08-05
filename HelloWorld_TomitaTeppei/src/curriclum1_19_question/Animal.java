0e1ae188c25bdb537f8bd6d60b80689740ef98ac
atimo <atimo@192.168.10.109>
2025-08-05 13:40:16 +0900

    修正

--- HelloWorld_TomitaTeppei/src/curriclum1_19_question/Animal.java
+++ HelloWorld_TomitaTeppei/src/curriclum1_19_question/Animal.java
@@ -4,23 +4,13 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class Animal {
-	static class Dog {
-		String name;
-		int count;
-
-		public Dog(String name, int count) {
-			this.name = name;
-			this.count = count;
-
-		}
-	}
 
 	public static void main(String[] args) {
-		Dog dog = new Dog("ポチ", 4);
+		Dog dog = new Dog();
 		// Q1：Dogクラスを呼び出して変数に代入し、動物の名前をコンソールに出力してください。
-		System.out.println("名前:"+dog.name);
+		System.out.println("名前:" + dog.name);
 		// Q2：Dogクラスを呼び出して変数に代入し、動物の数をコンソールに出力してください。
-		System.out.println("数:"+dog.count);
+		System.out.println("数:" + dog.count);
 		// Q3：現在の日時を「yyyy-MM-dd H:m:s」形式で出力
 		LocalDateTime now = LocalDateTime.now();
 		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd H:m:s");
