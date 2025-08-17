50821796ee487b001c4420bd36f08de456bd4936
atimo <atimo@192.168.10.109>
2025-08-17 14:38:40 +0900

    curriculum1_20,1_22

---
+++ Curriculum1,20_1,22/src/curriculum1_20_1_22/jikkou.java
@@ -0,0 +1,8 @@
+package curriculum1_20_1_22;
+
+public class jikkou {
+	public static void main(String[] args) {
+        syori syori = new syori();
+        syori.showInfo("寿司");
+    }
+}
---
+++ Curriculum1,20_1,22/src/curriculum1_20_1_22/syori.java
@@ -0,0 +1,46 @@
+package curriculum1_20_1_22;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class syori {
+	String where;
+	String taste;
+	String food;
+	String formattedDateTime;
+	
+	public syori() {
+		this.where = "日本";
+		this.taste = "うまい";
+		this.food = "和食";
+		
+		LocalDateTime now = LocalDateTime.now();
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd H:m:s");
+		this.formattedDateTime = now.format(formatter);
+
+	}
+	
+	public void showInfo(String specificFood) {
+		System.out.println("こんにちは！ここは" + this.where + "です！");
+        System.out.println("この" + specificFood + "は" + this.taste);
+        System.out.println(specificFood + "は" + this.food + "です");
+        System.out.println("今の現在日時は" + this.formattedDateTime + "です");
+	}
+}
+
+/*
+
+下記がコンソールに出力されるように作成してください
+
+	こんにちは！ここは日本です！
+	この寿司はうまい
+	寿司は和食です
+	今の現在日時は2023/03/09 10:23:39です
+
+【条件】
+	・JavaProjectを新規作成し、実行用のpackageと処理用のpackageを作成してください。
+	・適切なファイルにフィールドで変数を必要な数作ってください。
+	・thisを使って作成してください。
+	・日時は今日の日付を取得してください。
+	
+*/
