b82ecf51813037d427dfcf37fbef40eb338a4301
atimo <atimo@192.168.10.109>
2025-08-26 19:05:33 +0900

    修正

--- HelloWorld_TomitaTeppei/src/curriculum1_23/Animal.java
+++ HelloWorld_TomitaTeppei/src/curriculum1_23/Animal.java
@@ -1,1 +1,49 @@
 package curriculum1_23;
+
+public class Animal {
+	private String name;
+	private double bodyLength;
+	private int speed;
+
+	public Animal(String name, double bodyLength, int speed) {
+		this.name = name;
+		this.bodyLength = bodyLength;
+		this.speed = speed;
+	}
+
+	public String getName() {  //nameのgetter
+		return this.name;
+	}
+
+	public void setName(String name) {  //nameのsettter
+		this.name = name;
+	}
+
+	public double getBodyLength() {
+		return this.bodyLength;
+	}
+
+	public void setBodyLength(double bodyLength) {
+		this.bodyLength = bodyLength;
+	}
+
+	public int getSpeed() {
+		return this.speed;
+	}
+
+	public void setSpeed(int speed) {
+		this.speed = speed;
+	}
+
+}
+
+/*
+	
+	下記がコンソールに出力されるように作成してください
+	※thisとsetterとgetterとフィールドを使ってください
+	
+	動物名：ライオン
+	体長：2.1m
+	速度：80km/h
+	
+*/
--- HelloWorld_TomitaTeppei/src/curriculum1_23/Main.java
+++ HelloWorld_TomitaTeppei/src/curriculum1_23/Main.java
@@ -1,5 +1,13 @@
 package curriculum1_23;
 
 public class Main {
+	public static void main(String[] args) {
+		Animal lion = new Animal("ライオン", 2.1, 80);
 
+		System.out.println("動物名：" + lion.getName());
+		System.out.println("体長：" + lion.getBodyLength() + "m");
+		System.out.println("速度：" + lion.getSpeed() + "km/h");
+	}
 }
+
+
